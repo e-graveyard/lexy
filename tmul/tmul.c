efdcc6b1caf58b514f0887b5a27c86e6279ce3b0
Caian R. Ertl <caianrais@protonmail.com>
2018-12-27 01:06:45 -0200

    (C4) feat: implement the R(ead) loop

--- tmul/tmul.c
+++ tmul/tmul.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 
+static char input[2048];
+
 int main()
 {
+    puts("TMUL: Version 0.1.0");
+    puts("Press CTRL+c to Exit\n");
+
+    while(1)
+    {
+        fputs("tmul > ", stdout);
+
+        fgets(input, 2048, stdin);
+
+        printf("=> %s", input);
+    }
+
     return 0;
 }
