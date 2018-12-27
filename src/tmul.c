1017165ced4d692e769d5f705c77241f7c96e307
Caian R. Ertl <caianrais@protonmail.com>
2018-12-27 02:03:28 -0200

    (C4) feat: add Windows compatibility

--- src/tmul.c
+++ src/tmul.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// If we are compiling on Windows...
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt)
+{
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) -1] = '\0';
+
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+// Otherwise, include the 'editline' headers.
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
+
 
 int main()
 {
