77f4a99ea90ef15455a641268552f1b776564051
Caian R. Ertl <caianrais@protonmail.com>
2018-12-27 02:49:50 -0200

    (C6) feat: implement a basic parser for polish notation

--- src/parser.c
+++ src/parser.c
@@ -32,6 +32,20 @@ void add_history(char* unused) {}
 
 int main()
 {
+    mpc_parser_t* Number     = mpc_new("numb");
+    mpc_parser_t* Operator   = mpc_new("oper");
+    mpc_parser_t* Expression = mpc_new("expr");
+    mpc_parser_t* Tmul       = mpc_new("tmul");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+            " \
+                numb: /-?[0-9]+/ ;                      \
+                oper: '+' | '-' | '*' | '/' ;           \
+                expr: <numb> | '(' <oper> <expr>+ ')' ; \
+                tmul: /^/ <oper> <expr>+ /$/ ;          \
+            ",
+            Number, Operator, Expression, Tmul);
+
     puts("TMUL: Version 0.1.0");
     puts("Press CTRL+c to Exit\n");
 
@@ -40,9 +54,22 @@ int main()
         char* input = readline("tmul > ");
         add_history(input);
 
-        printf("=> %s\n\n", input);
+        mpc_result_t r;
+        if(mpc_parse("<stdin>", input, Tmul, &r))
+        {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else
+        {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expression, Tmul);
 
     return 0;
 }
