357fb999e57a03fd363368f2974eab1364993f32
Caian R. Ertl <caianrais@protonmail.com>
2018-12-28 00:29:33 -0200

    (C8) feat: include some basic structures to describe responses and errors

--- src/eval.h
+++ src/eval.h
@@ -2,6 +2,27 @@
 #include <math.h>
 #include "mpc.h"
 
+enum
+{
+    TLVAL_NUM,
+    TLVAL_ERR
+};
+
+enum
+{
+    LERR_DIV_ZERO,
+    LERR_BAD_OP,
+    LERR_BAD_NUM
+};
+
+typedef struct
+{
+    int type;
+    int error;
+    float number;
+}
+tlval;
+
 bool equals(char* ref, char* txt);
-float eval_op(char* op, float x, float y);
-float eval(mpc_ast_t* t);
+tlval eval_op(char* op, tlval x, tlval y);
+tlval eval(mpc_ast_t* t);
