b1828639ed59217696aa41bd5051233197c34185
Caian R. Ertl <caianrais@protonmail.com>
2019-01-03 01:01:06 -0200

    (C10) feat: include a parser for Q-Expressions

--- src/parser.c
+++ src/parser.c
@@ -7,32 +7,37 @@ parser_T init_parser()
     p.Number = mpc_new("numb");
     p.Symbol = mpc_new("symb");
     p.SExpr  = mpc_new("sexp");
+    p.QExpr  = mpc_new("qexp");
     p.PExpr  = mpc_new("pexp");
     p.TLisp  = mpc_new("lisp");
 
     mpca_lang(MPCA_LANG_DEFAULT,
 
-            " numb: /-?[0-9]+\\.?[0-9]*/ ;     "
-            "                                  "
-            " symb: ('+' | \"add\") |          "
-            "       ('-' | \"sub\") |          "
-            "       ('*' | \"mul\") |          "
-            "       ('/' | \"div\") |          "
-            "       ('%' | \"mod\") |          "
-            "       ('^' | \"pow\") |          "
-            "       \"sqrt\" |                 "
-            "       \"min\" |                  "
-            "       \"max\" ;                  "
-            "                                  "
-            " sexp: '(' <pexp>* ')' ;          "
-            "                                  "
-            " pexp: <numb> | <symb> | <sexp> ; "
-            "                                  "
-            " lisp: /^/ <pexp>* /$/ ;          ",
+            " numb: /-?[0-9]+\\.?[0-9]*/ ; "
+            "                              "
+            " symb: ('+' | \"add\") |      "
+            "       ('-' | \"sub\") |      "
+            "       ('*' | \"mul\") |      "
+            "       ('/' | \"div\") |      "
+            "       ('%' | \"mod\") |      "
+            "       ('^' | \"pow\") |      "
+            "       \"sqrt\" |             "
+            "       \"min\" |              "
+            "       \"max\" ;              "
+            "                              "
+            " sexp: '(' <pexp>* ')' ;      "
+            "                              "
+            " qexp: \"'(\" <pexp>* ')' ;   "
+            "                              "
+            " pexp: <numb> | <symb> |      "
+            "       <sexp> | <qexp> ;      "
+            "                              "
+            " lisp: /^/ <pexp>* /$/ ;      ",
 
             p.Number,
             p.Symbol,
             p.SExpr,
+            p.QExpr,
             p.PExpr,
             p.TLisp);
 
--- src/parser.h
+++ src/parser.h
@@ -5,6 +5,7 @@ typedef struct parser_S
     mpc_parser_t* Number;
     mpc_parser_t* Symbol;
     mpc_parser_t* SExpr;
+    mpc_parser_t* QExpr;
     mpc_parser_t* PExpr;
     mpc_parser_t* TLisp;
 }
