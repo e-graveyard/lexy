2202759f6e574d5fef61813b6274d5e84fae5a68
Caian R. Ertl <caianrais@protonmail.com>
2019-01-19 08:24:10 -0200

    feat: implement the hashmap library

---
+++ src/hash.h
@@ -0,0 +1,13 @@
+struct ht_item_S;
+struct ht_index_S;
+typedef struct ht_item_S ht_item_T;
+typedef struct ht_index_S ht_index_T;
+
+
+
+ht_index_T* ht_new();
+void ht_destroy(ht_index_T* t);
+
+void  ht_insert(ht_index_T* t, const char* k, const char* v);
+char* ht_search(ht_index_T* t, const char* k);
+void  ht_delete(ht_index_T* t, const char* k);
