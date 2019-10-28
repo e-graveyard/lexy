e7310a9f38f5eb95c506191e41853a80d1ba1a1f
Caian R. Ertl <hi@caian.org>
2019-10-28 01:16:50 -0300

    feat: share more eval functions

--- src/eval.h
+++ src/eval.h
@@ -8,11 +8,16 @@
 #define ERROR_MESSAGE_BYTE_LENGTH 512
 
 // ...
-void     tlenv_init (tlenv_T* env);
-tlenv_T* tlenv_new  (void);
-void     tlval_del  (tlval_T* v);
-tlval_T* tlval_read (mpc_ast_t* t);
-tlval_T* tlval_eval (tlenv_T* env, tlval_T* value);
-int      tlval_eq   (tlval_T* a, tlval_T* b);
+int      tlval_eq    (tlval_T* a, tlval_T* b);
+void     tlval_print (tlval_T* t);
+void     tlenv_init  (tlenv_T* env);
+void     tlval_del   (tlval_T* v);
+tlenv_T* tlenv_new   (void);
+tlval_T* tlval_sexpr (void);
+tlval_T* tlval_read  (mpc_ast_t* t);
+tlval_T* tlval_add   (tlval_T* v, tlval_T* x);
+tlval_T* tlval_eval  (tlenv_T* env, tlval_T* value);
+tlval_T* tlval_err   (const char* fmt, ...);
+tlval_T* tlval_str   (char* s);
 
 #endif
