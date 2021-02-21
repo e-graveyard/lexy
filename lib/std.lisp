33f310062d56b3fe249d49a84f7df13a693ccaef
Caian R. Ertl <hi@caian.org>
2021-02-21 05:13:17 -0300

    chore: std lib additions

--- lib/std.lisp
+++ lib/std.lisp
@@ -1,4 +1,25 @@
+; boolean types
+(let {true} 1)
+(let {false} 0)
+
+; aliases
 (let {+} add)
 (let {-} sub)
 (let {*} mul)
 (let {/} div)
+
+; function definition
+(let {fn}
+  (lambda {args body}
+    {let (head args) (lambda (tail args) body)}))
+
+; currying
+(let {unpack}
+  (lambda {f args} {eval (join (list f) args)}))
+
+(let {curry} unpack)
+
+(let {pack}
+  (lambda {f & xs} {f xs}))
+
+(let {uncurry} pack)
